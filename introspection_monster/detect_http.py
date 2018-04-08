9d0726d637a6fdf65923a2f7755e9157f499e7c1
David Albone <davd.p.albone@gmail.com>
2018-04-08 08:33:52 +0100

    Added http finding code

---
+++ introspection_monster/detect_http.py
@@ -0,0 +1,29 @@
+import re
+
+
+REGEX_TEMPLATE = 'http:\/\/.*{replacement}[^\s]+'
+
+
+class DetectHttp(object):
+    def detect_http(self, detection_string, string_to_test):
+        current_template = REGEX_TEMPLATE.replace('{replacement}', detection_string)
+
+        all_matching_values = set()
+
+        # split the string so that multiple URLs can be detected
+        split_string = string_to_test.split()
+
+        for _ in split_string:
+            matches = re.search(current_template, _)
+            if matches is not None:
+                all_matching_values.add(matches.group(0))
+
+        return all_matching_values
+
+
+if __name__ == '__main__':
+    dh = DetectHttp()
+    results = dh.detect_http('bbc', 'Read the news at http://www.bbc.co.uk or at http://www.bbc.com')
+    print(results)
+    for result in results:
+        print(result)
---
+++ tests/test_detect_http.py
@@ -0,0 +1,37 @@
+import pytest
+
+from introspection_monster.detect_http import DetectHttp
+
+
+@pytest.fixture
+def set_up():
+    detect_http = DetectHttp()
+    return detect_http
+
+
+def test_http_detected_http_url_only(set_up):
+    assert set_up.detect_http('bbc', 'http://www.bbc.co.uk') == {'http://www.bbc.co.uk'}
+
+
+def test_http_detected_htp_url_and_string(set_up):
+    assert set_up.detect_http('bbc', 'Read the news at http://www.bbc.co.uk') == {'http://www.bbc.co.uk'}
+
+
+def test_http_detected_multiple_http_urls(set_up):
+    assert set_up.detect_http('bbc', 'Read the news at http://www.bbc.co.uk or at http://www.bbc.com') == {'http://www.bbc.co.uk', 'http://www.bbc.com'}
+
+
+def test_no_url(set_up):
+    assert set_up.detect_http('bbc', 'Read the news at the bbc') == set()
+
+
+def test_only_https(set_up):
+    assert set_up.detect_http('bbc', 'Read the news at https://www.bbc.co.uk') == set()
+
+
+def test_multiple_https(set_up):
+    assert set_up.detect_http('bbc', 'Read the news at https://www.bbc.co.uk or at https://www.bbc.com') == set()
+
+
+def test_mixture_of_http_and_https(set_up):
+    assert set_up.detect_http('bbc', 'Read the news at http://www.bbc.co.uk or at https://www.bbc.com') == {'http://www.bbc.co.uk'}
